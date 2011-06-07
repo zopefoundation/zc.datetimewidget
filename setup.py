221dd5da0a0357abb2777f766f44a16f3fee0496
Gediminas Paulauskas <menesis@pov.lt>
2011-06-07 15:24:45 +0000

    Back to development: 0.7.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zc.datetimewidget',
-      version='0.7.0',
+      version='0.7.1dev',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Javascript-based widgets for date and datetime fields.',
